9d2d2a85fe542fd4a39c38033ca35d2351eca7ea
frantzmoseau <frantzmoseau35@gmail.com>
2023-01-13 21:36:54 -0500

    commit tree 6f039683c9c2646226dd42e065d94fbdadcd1fad

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 
-  const record = [
-    { year: "2018", result: "N/A"},
-    { year: "2017", result: "N/A"},
-    { year: "2016", result: "N/A"},
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    { year: "2012", result: "N/A"},
-    { year: "2011", result: "N/A"}
+  // const record = [
+  //   { year: "2018", result: "N/A"},
+  //   { year: "2017", result: "N/A"},
+  //   { year: "2016", result: "N/A"},
+  //   { year: "2015", result: "W"},
+  //   { year: "2014", result: "N/A"},
+  //   { year: "2013", result: "L"},
+  //   { year: "2012", result: "N/A"},
+  //   { year: "2011", result: "N/A"}
  
-  ]
+  // ]
 
 
-
-function superbowlWin(record) {
-  for(let item of record) {
-    if( item.result === "W"){
-      return item.year;
-    }
-  }
+function superbowlWin(arr) {
+ const answer = arr.find(team => team.result === 'W')
+ if (answer === true) {
+  return answer.year
+ }
+ else {
+   undefined
+ }
 }
